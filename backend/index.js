ac8affc3a09d3acc4ad4a890a64b2a7d3d917b7e
MRajczyk <mikolajrajczyk01@gmail.com>
2023-10-20 13:46:40 +0200

    fix docker compose again so that db is visible from adminer container

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+})
+
+app.listen(port, () => {
+    console.log('listening at port ' + port);
+});
+
