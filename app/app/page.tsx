a4a801f1ae040abc5342826f9b6dbfe2a2603e60
MRajczyk <mikolajrajczyk01@gmail.com>
2023-11-17 22:57:36 +0100

    add button component + utils + new packages

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/server/auth";
 import { LoginButton, LogoutButton } from "@/components/auth";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -12,6 +13,7 @@ export default async function Home() {
       <p>Hello world, yes!</p>
       <pre>{JSON.stringify(session)}</pre>
       {session ? <p>Logged in</p> : <p>Not logged in</p>}
+      <Button variant="destructive">Test</Button>
     </main>
   );
 }
