d466c13b4ba50cfca1045f3716ae8fca7188734e
MRajczyk <mikolajrajczyk01@gmail.com>
2023-12-03 20:46:10 +0100

    fix warning from server

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={"h-full w-full"}>
+    <html lang="en">
       <body className={"h-full w-full"}> {children}</body>
     </html>
   );
