ac5df875b06a331f564ecafe07682616571d2075
MRajczyk <mikolajrajczyk01@gmail.com>
2023-12-07 19:29:32 +0100

    tab css fun

--- app/components/profile/profileTab.tsx
+++ app/components/profile/profileTab.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 const ProfileTab = ({ title, href }: { title: string; href: string }) => {
   return (
-    <div className="flex bg-orange-100 w-[90%] h-[80px] items-center justify-center rounded-xl">
+    <Button
+      asChild
+      className="flex bg-orange-100 w-[90%] h-[80px] items-center justify-center rounded-xl"
+    >
       <Link className="text-xl" href={href}>
         {title}
       </Link>
-    </div>
+    </Button>
   );
 };
 
