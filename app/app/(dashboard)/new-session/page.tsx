2c576b935261747119f18929550823211fb05e3e
MRajczyk <mikolajrajczyk01@gmail.com>
2023-12-08 20:14:39 +0100

    add todo doc + start implementing new session page

--- app/app/(dashboard)/new-session/page.tsx
+++ app/app/(dashboard)/new-session/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const NewSessionPage = () => {
-  return <div>NewSessionPage Works!</div>;
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="text-4xl p-8">
+        <b>Nowe wędzenie</b>
+      </div>
+      <div className="grid grid-cols-2 w-full h-full">
+        <div className="flex bg-yellow-100 border-2 border-red-600 mx-4 mb-4">
+          Down Left
+        </div>
+        <div className="flex bg-green-100 border-2 border-red-600 mx-4 mb-4">
+          Down Right
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewSessionPage;
