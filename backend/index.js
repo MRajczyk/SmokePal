7880e0b0863d5b901e2c0f31c701c8b8ae52dc09
MRajczyk <mikolajrajczyk01@gmail.com>
2023-12-11 18:00:19 +0100

    change session schema to accomodate new idea

--- backend/index.js
+++ backend/index.js
@@ -122,6 +122,7 @@ app.post("/api/start", async (req, res) => {
   try {
     const newSession = await prisma.smokingSession.create({
       data: { authorId: req.body.authorId },
+      finished: false,
     });
     currentSessionId = newSession.id;
     if (!currentSessionId) {
