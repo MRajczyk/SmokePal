4b719416f1635c9dc0a51e2a947b5d4fd84bfaad
MRajczyk <mikolajrajczyk01@gmail.com>
2024-01-03 19:22:49 +0100

    dashboard planning + forecast data sample response downloaded

--- app/app/(dashboard)/page.tsx
+++ app/app/(dashboard)/page.tsx
@@ -2,5 +2,17 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home component works!</div>;
+  return (
+    <div className="grid grid-cols-5 w-full">
+      <div className="flex col-span-3 row-span-1 w-full p-4 pr-2 pb-2">
+        <div className="w-full bg-slate-300">Zaczynamy wędzić!</div>
+      </div>
+      <div className="flex col-span-2 row-span-2 w-full p-4 pl-2">
+        <div className="w-full bg-red-300">Pogoda</div>
+      </div>
+      <div className="flex col-span-3 row-span-2 w-full p-4 pr-2 pt-2">
+        <div className="w-full bg-orange-300">Twoje ostatnie wędzenie</div>
+      </div>
+    </div>
+  );
 }
