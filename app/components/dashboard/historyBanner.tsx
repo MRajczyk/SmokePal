383ce1be8f8d8b57dbf0728c636ea354328f8e89
MRajczyk <mikolajrajczyk01@gmail.com>
2024-01-06 10:29:47 +0100

    move history button text to left

--- app/components/dashboard/historyBanner.tsx
+++ app/components/dashboard/historyBanner.tsx
@@ -14,7 +14,7 @@ const HistoryBanner = () => {
         variant={"ghost"}
       >
         <Link href="/history" className="flex text-[#F4EDE5]">
-          <span className="flex flex-col gap-2 text-4xl mr-2 w-full break-words text-wrap whitespace-normal">
+          <span className="flex flex-col gap-2 text-4xl mr-2 w-full break-words text-wrap whitespace-normal text-start">
             Smoking history
             <p className="text-base font-normal w-full break-words text-wrap whitespace-normal">
               Go to history of your smoking sessions and track your flavorful
